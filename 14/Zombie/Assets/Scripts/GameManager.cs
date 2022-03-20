d96b65fd8df756cf7cd4147918f06ea097c7b164
sheon <sheon1206@gmail.com>
2022-03-20 20:03:40 +0900

    GameManager Update

--- 14/Zombie/Assets/Scripts/GameManager.cs
+++ 14/Zombie/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // 점수와 게임 오버 여부를 관리하는 게임 매니저
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
     // 싱글톤 접근용 프로퍼티
     public static GameManager instance
     {
@@ -21,8 +23,28 @@ public class GameManager : MonoBehaviour {
 
     private static GameManager m_instance; // 싱글톤이 할당될 static 변수
 
+    public GameObject playerPreFeb; // 생성할 플레이어 케릭터 프리팹
+
     private int score = 0; // 현재 게임 점수
     public bool isGameover { get; private set; } // 게임 오버 상태
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		// 로컬 오브젝트라면 쓰기 부분이 실행됨
+		if (stream.IsWriting)
+		{
+            // 네트워크를 통해 score값 보내기
+            stream.SendNext(score);
+		}
+		else
+		{
+            // 네트워크를 통해 score값 받기
+            score = (int)stream.ReceiveNext();
+
+            // 동기화하여 받은 점수를 UI로 표시
+            UIManager.instance.UpdateScoreText(score);
+		}
+	}
 
     private void Awake() {
         // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
@@ -34,8 +56,13 @@ public class GameManager : MonoBehaviour {
     }
 
     private void Start() {
-        // 플레이어 캐릭터의 사망 이벤트 발생시 게임 오버
-        FindObjectOfType<PlayerHealth>().onDeath += EndGame;
+        // 생성할 랜덤 위치 지정
+        Vector3 randomSpawnPos = Random.insideUnitSphere * 5f;
+        randomSpawnPos.y = 0;
+
+        // 네트워크상의 모든 클라이언트에서 생성 실행
+        // 해당 게임 오브젝트의 주도권은 생성 메서드를 직접 실행한 클라이언트에 있음
+        PhotonNetwork.Instantiate(playerPreFeb.name, randomSpawnPos, Quaternion.identity);
     }
 
     // 점수를 추가하고 UI 갱신
@@ -57,4 +84,18 @@ public class GameManager : MonoBehaviour {
         // 게임 오버 UI를 활성화
         UIManager.instance.SetActiveGameoverUI(true);
     }
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            PhotonNetwork.LeaveRoom();
+		}
+	}
+
+	// 룸을 나갈 때 자동 실행되는 메서드
+	public override void OnLeftRoom()
+	{
+        SceneManager.LoadScene("Lobby");
+	}
 }
